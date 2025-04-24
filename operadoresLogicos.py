a558850fd18a5bf23d413de2697ca235f9c0b7c0
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-24 14:08:48 +0200

    operador and

--- operadoresLogicos.py
+++ operadoresLogicos.py
@@ -3,3 +3,12 @@ negacion = not calor
 
 print(f'inicial: {calor}')
 print(f'negacion de la proposicion inicial: {negacion}')
+
+proposicion1 = True
+proposicion2 = False
+
+conjuncion = proposicion1 and proposicion2
+
+print(f'proposicion1: {proposicion1}')
+print(f'proposicion2: {proposicion2}')
+print (f'{conjuncion}')
