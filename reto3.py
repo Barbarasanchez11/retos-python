e4806c46b475daea4a688a1ae9d228e24dcf17a1
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-24 11:11:57 +0200

    cuadro de tenis

--- reto3.py
+++ reto3.py
@@ -1,10 +1,33 @@
 import random
 
-jugadores = ['Alcaraz', 'Djokovic', 'Zverez', 'Medvedev', 'Tsisipas', 'Rune', 'Rublev', 'Fritz']
 
-def partido(j1,j2):
-    ganador = random.choice([j1,j2])
-    print(f'{j1} vs {j2} Ganador:{ganador}')
+jugadores = ['Alcaraz', 'Djokovic', 'Zverev', 'Medvedev', 'Tsitsipas', 'Rune', 'Rublev', 'Fritz']
+
+
+def partido(j1, j2):
+    ganador = random.choice([j1, j2])
+    print(f'{j1} vs {j2} , Ganador: {ganador}')
     return ganador
 
-partido('Alcaraz','Djokovic')
+
+print('Cuartos de final:')
+semi_finalistas = []
+for i in range(0, len(jugadores), 2):
+    ganador = partido(jugadores[i], jugadores[i+1])
+    semi_finalistas.append(ganador)
+
+
+print('Semifinales:')
+finalistas = []
+for i in range(0, len(semi_finalistas), 2):
+    ganador = partido(semi_finalistas[i], semi_finalistas[i+1])
+    finalistas.append(ganador)
+
+
+print('Final:')
+campeon = partido(finalistas[0], finalistas[1])
+
+
+print(f'El campeón del torneo es: {campeon.upper()}!')
+
+      
