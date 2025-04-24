c31590e9fdce72887ae54f6ab9cc991ce538eae7
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-24 12:39:53 +0200

    tipos de datos

---
+++ datos.py
@@ -0,0 +1,12 @@
+pokemon = ["Charichard", "Ponita"] #Lista [], las listas se pueden modificar
+pokemon.append("Snorlax")
+pokemon.append("Arceus")
+pokemon.sort()
+print(pokemon)
+
+flores = ("Rosas", "Orquideas", "Tulipanes", "Margaritas") #TUPLA (), la tupla no se puede modificar
+print(flores)
+
+persona = { "nombre":"Pipo", "especie":"perrito"} #Diccrionario contiene elementos como clave-valor relacioonados entre ellos
+
+print(persona["especie"])
