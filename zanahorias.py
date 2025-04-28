b785bf631503cd4bd976bd1f9c18852e843dddda
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-28 10:07:59 +0200

    ejercicio zanahorias

---
+++ zanahorias.py
@@ -0,0 +1,29 @@
+#Un grupo de agricultores está participando en un concurso para ver quién tiene las zanahorias más grandes. Han medido el peso (en gramos) de 12 zanahorias y necesitan analizar los datos para ver qué tan uniformes son sus cultivos.
+
+
+#📌 Tu misión
+#📊 Escribe un programa en Python que haga lo siguiente:
+#Calcule la media (promedio) del peso de las zanahorias.
+#Encuentre la mediana del peso.
+#Determine la moda (peso más común).Calcule la desviación estándar para ver qué tanto varían los pesos.
+#zanahorias = [300, 280, 290, 310, 275, 290, 295, 315, 290, 280, 310, 305]
+#📊 Extra 1: Agrega una visualización con Matplotlib 📉 para mostrar la distribución de los pesos.
+#🤖 Extra 2: Modifica el código para que el usuario pueda ingresar los pesos de nuevas zanahorias.
+
+import numpy as np
+import statistics as stats
+#import matplotlib.pyplot as plt
+
+zanahorias = [300, 280, 290, 310, 275, 290, 295, 315, 290, 280, 310, 305]
+
+media = np.mean(zanahorias)
+mediana = np.median(zanahorias)
+moda= stats.mode(zanahorias)
+desviacion= np.std(zanahorias)
+
+print(f'media, {media}')
+print(f'mediana, {mediana}')
+print(f'moda, {moda}')
+print(f'desviacion, {desviacion:.2f}')
+
+
