e86e2f93b5a5eb7db3095959266ae0eac578dd55
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-29 13:10:56 +0200

    primera parte ejercicio pandas

---
+++ pandas.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+#cargar un archivo
+
+df = pd.read_csv("ventas.csv")
+
+df.head()
+
+df['total'] = df['precio'] * df['unidades']
+df.head()
+
+df.groupby('producto')['total'].sum()
+df.head()
+
+#filtro por unidades
+
+df[df['unidades'] > 3]
+
+#fecha
+
+df['fecha'] = pd.to_datetime(df['fecha'])
+
+df['mes'] = df['fecha'].dt.month
+
+df.head()
+
+#agrupar por mes
+
+df.groupby('mes')['total'].sum()
