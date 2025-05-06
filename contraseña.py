5ba75e72305f291e92b5776c27ca68be21cc3bfd
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-05-06 16:04:49 +0200

    comment added

--- contraseña.py
+++ contraseña.py
@@ -1,12 +1,11 @@
 contraseña = 'python'
 intento = input('Introduce la contraseña: ')
 
-
+#bucle while 
 while intento != contraseña :
    print('Contraseña incorreta')
    intento = input('Introduce la contraseña: ')
     
 print('Contraseña correcta')  
    
-  
   
